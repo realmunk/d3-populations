e8ccd088d73d149fdace1a90d792fbde1df6028a
Kristian S. Munkvold <ksm@computas.com>
2015-10-15 19:34:57 +0200

    Setup before presentation

---
+++ source/table-of-contents.js
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////
+//                                                           //
+//             datavisualisering med d3.js                   //                              
+//                                                           //
+//              PART 1: @LIGHTWEIGHT graph                   //
+//                      1. SVG                               //
+//                      2. RECT                              //
+//                      3. SCALE                             //
+//                      4. X-AXIS                            //
+//                                                           //
+//              PART 2: @EASY graph                          //
+//                      5. UPDATE SCALE                      //
+//                      6. UPDATE FIRST RECT AND POSITION    //
+//                      7. ADD RECT AND POSITION             // 
+//                                                           //
+//              PART 3: @FLEXIBLE graph                      //
+//                     8. ARRAY AND FOREACH                  //
+//                     9. REFACTOR APPENDING                 //
+//                     10. SCALEY                            //
+//                     11. LOAD ANOTHER POPULATION           //
+//                                                           //
+//              PART 4: @ADVANCED graph                      //
+//                     12. LOAD A MORE ADVANCED POPULATION   //
+//                     13. TALK ABOUT THE DATA               //
+//                                                           //
+//              PART 5: @RESPONSIVE GRAPH                    //
+//                     14. MAKE IT RESPONSIVE                //
+//                                                           //
+///////////////////////////////////////////////////////////////   
